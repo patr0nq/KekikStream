f75562901c63cea52b725523ac742b65e6182402
keyiflerolsun <keyiflerolsun@gmail.com>
2026-02-17 07:05:00 +0300

    feat: Add EpikPlayer extractor for streaming services

---
+++ KekikStream/Extractors/EpikPlayer.py
@@ -0,0 +1,7 @@
+# Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+from KekikStream.Core import BePlayerExtractor
+
+class EpikPlayer(BePlayerExtractor):
+    name     = "EpikPlayer"
+    main_url = "https://epikplayer.xyz"
